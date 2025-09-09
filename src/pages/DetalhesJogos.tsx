9abffc425c9deae70c5419f7724c35e9e4d4962f
evertonWithNoCode <everton@withnocode.io>
2025-09-09 12:09:06 -0300

    tela jogos feita

--- src/pages/DetalhesJogos.tsx
+++ src/pages/DetalhesJogos.tsx
@@ -1,5 +1,9 @@
 import { AppSidebar } from "@/components/AppSideBar";
 import React, { useState } from "react";
+import { FaCirclePlay } from "react-icons/fa6";
+import { FaRegPaperPlane } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import { IoIosArrowBack } from "react-icons/io";
 
 const DetalhesJogos: React.FC = () => {
     const [reviewText, setReviewText] = useState("");
@@ -40,30 +44,31 @@ const DetalhesJogos: React.FC = () => {
     return (
         <main className="min-h-screen bg-[#FFFCF2] flex w-full font-sans text-gray-800 ">
             {/* Header */}
-            <AppSidebar/>
+            <AppSidebar />
             <div className="flex flex-col w-full">
-                <div className="flex items-center gap-2  ml-10 mt-10">
-                        <svg
-                            width="24"
-                            height="24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M15 18L9 12L15 6"
-                                stroke="#F7B34D"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
+                <div className="flex items-center gap-2 justify-between ml-10 mr-[200px] mt-10 ">
+                    <div className="flex gap-2 items-center">
+                        <IoIosArrowBack className="text-[#FBBF24] text-xl"/>
                         <span className="text-[#2C2623] font-extrabold text-base">
                             Galeria de jogos
                         </span>
                     </div>
+
+                    <div className="flex gap-6">
+                        {/* Ícone de coração */}
+                        <div className="w-[50px] h-[50px] flex items-center justify-center rounded-[40%] border-[2px] border-[#FFF5D6] bg-white shadow-sm">
+                            <FaRegHeart className="text-[#FBBF24] text-xl" />
+                        </div>
+
+                        {/* Ícone de aviãozinho */}
+                        <div className="w-[50px] h-[50px] flex items-center justify-center rounded-[40%] border-[2px] border-[#FFF5D6] bg-white shadow-sm">
+                            <FaRegPaperPlane className="text-[#FBBF24] text-xl" />
+                        </div>
+                    </div>
+                </div>
                 <header className="w-full max-w-5xl mx-auto ">
                     {/* Voltar */}
-                
+
                     {/* Título + Categorias */}
                     <div className="flex flex-col items-center text-center gap-6 mb-[150px]">
                         <h1 className="text-[#2C2623] text-4xl md:text-5xl font-extrabold leading-tight">
@@ -100,7 +105,14 @@ const DetalhesJogos: React.FC = () => {
                 <div className="bg-[#FEF2CC]  relative h-[1300px] pt-[500px] w-full">
                     <div className="w-[864px] h-[487px] absolute bottom-[870px] left-1/2 -translate-x-1/2 max-md:w-full max-md:bottom-[00px]">
                         <div className="w-[864px] h-[487px] shadow-[0_3px_0_0_#FFE0B2] absolute bg-white rounded-3xl border-2 border-[#FBDEB1] max-md:w-full" />
-                        <div className="w-[824px] h-[447px] absolute bg-[#FEF2CC] rounded-2xl left-5 top-5 max-md:w-full" />
+                        <div className="w-[824px] h-[447px] absolute bg-[#FEF2CC] rounded-2xl left-5 top-5 max-md:w-full flex items-center justify-center">
+                            <button className="flex items-center justify-center w-[248px] h-[64px] gap-2 bg-[#FBB040] text-white font-bold text-lg  rounded-full shadow-md hover:opacity-90 transition">
+                                {/* Ícone Play */}
+                                <FaCirclePlay color="white" />
+                                Iniciar jogo
+                            </button>
+                        </div>
+
                     </div>
                     <section className=" flex justify-center gap-[180px]  py-10">
                         {/* Objetivos */}
